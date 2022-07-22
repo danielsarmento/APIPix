c530c221842dc849716d65731c8ac097a65ea87b
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-07-22 14:51:26 -0300

    certificação

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const fs = require("fs");
+const https = require("https");
+var logger = require('morgan');
+
+const httpsOptions = {
+    
+    ca: fs.readFileSync("chain-pix-prod.crt"),   // Certificado público da Gerencianet
+    minVersion: "TLSv1.2",
+    requestCert: true,
+    rejectUnauthorized: false, //Mantenha como false para que os demais endpoints da API não rejeitem requisições sem MTLS
+};
+
+const httpsServer = https.createServer(httpsOptions, app);
+app.use(logger('dev'));
+
 const routes = require('./routes/routes');
 const PORT = process.env.PORT;
 
