90ca18b9ac19459afee56a916040bc063b3f60ed
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-07-25 17:29:32 -0300

    Sucesso WhatsApp

--- controllers/controllerHook.js
+++ controllers/controllerHook.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
 
 exports.notificacao = async (req, res) => {
+    const e2e  = req.body.pix[0].endToEndId;
+    const txId = req.body.pix[0].txid;
+    const valor = req.body.pix[0].valor;
+    const horario = req.body.pix[0].horario;
+
     const token = 'n23pPyiaCzFAlaiXAGdrGWAWUTfSSO0uhCQM';
     const URL = 'https://api.zenvia.com/v2/channels/whatsapp/messages';
     const body = {
@@ -8,8 +13,11 @@ exports.notificacao = async (req, res) => {
         to: '5583999415087',
         contents: [
             {
-                type: 'text',
-                text: 'API funcionando'
+                type: 'template',
+                templateId: '66c87ec0-e194-45c1-b5d6-15b536f6a693',
+                fields: {
+                    txId: `${e2e}, com *txId:* ${txId}, no *valor de R$* ${valor} no *dia:* ${horario}, `
+                }
             }
         ]
     }
@@ -20,7 +28,7 @@ exports.notificacao = async (req, res) => {
                 'Content-Type': 'application/json',
             },
         });
-        console.log(response.data);
+        console.log(response);
         return res.send(response.data);
     } catch (error) {
         console.log(error);  
